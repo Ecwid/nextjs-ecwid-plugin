f38b7177f8a4c5f2f53bdca21b34e4d1c1056363
meteor <meteor@ecwid.com>
2024-01-28 23:24:42 +0400

    Compatibility for Next.js 14 #6

--- src/ProductBrowser.js
+++ src/ProductBrowser.js
@@ -1,51 +1,49 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {useEffect} from 'react'
+import { useEffect } from 'react'
 
-const ProductBrowser = (props) => {
+const ProductBrowser = ({ storeId = 13433173 }) => {
 
-  useEffect(() => {
+    useEffect(() => {
 
-    let ecwidLoaded = false;
+        let ecwidLoaded = false;
 
-    function load_ecwid() {
-      if( typeof Ecwid != 'undefined' ) {
-        Ecwid.OnAPILoaded.add(function() {
-          if (!ecwidLoaded) {
-            ecwidLoaded = true;
-            xProductBrowser("categoriesPerRow=3", "views=grid(3,3) list(10) table(20)", "categoryView=grid", "searchView=list", "id=ecStoreProductBrowser");
-          }
-        });
-      }
-    }
+        function load_ecwid() {
+            if (typeof Ecwid != 'undefined') {
+                Ecwid.OnAPILoaded.add(function () {
+                    if (!ecwidLoaded) {
+                        ecwidLoaded = true;
+                        xProductBrowser("categoriesPerRow=3", "views=grid(3,3) list(10) table(20)", "categoryView=grid", "searchView=list", "id=ecStoreProductBrowser");
+                    }
+                });
+            }
+        }
 
-    window.ec = window.ec || {};
-    window.ec.config = window.ec.config || {};
-    window.ec.config.storefrontUrls = window.ec.config.storefrontUrls || {};
-    window.ec.config.storefrontUrls.cleanUrls = true;
-    window.ec.config.storefrontUrls.queryBasedCleanUrls = true;
+        window.ec = window.ec || {};
+        window.ec.config = window.ec.config || {};
+        window.ec.config.storefrontUrls = window.ec.config.storefrontUrls || {};
+        window.ec.config.storefrontUrls.cleanUrls = true;
+        window.ec.config.storefrontUrls.queryBasedCleanUrls = true;
 
-    window.ecwid_script_defer = true;
-    window.ecwid_dynamic_widgets = true;
-    
-    if (!document.getElementById('ecwid-script')) {
-      var script = document.createElement('script');
-      script.charset = 'utf-8';
-      script.type = 'text/javascript';
-      script.src = 'https://app.ecwid.com/script.js?' + props.storeId + '&data_platform=nextjs';
-      script.id = 'ecwid-script'
-      script.onload = load_ecwid
-      document.body.appendChild(script);
-    } else {
-      load_ecwid()
-    }
-  })
+        window.ecwid_script_defer = true;
+        window.ecwid_dynamic_widgets = true;
 
-  return (
-    <div id="ecStoreProductBrowser"></div>
-  )
+        if (!document.getElementById('ecwid-script')) {
+            var script = document.createElement('script');
+            // script.charset = 'utf-8';
+            script.type = 'text/javascript';
+            script.src = 'https://app.ecwid.com/script.js?' + storeId + '&data_platform=nextjs';
+            script.id = 'ecwid-script'
+            script.onload = load_ecwid
+            document.body.appendChild(script);
+        } else {
+            load_ecwid()
+        }
+    })
+
+    return (
+        <div id="ecStoreProductBrowser"></div>
+    )
 }
-
-ProductBrowser.defaultProps = {storeId: 13433173};
 
 export default ProductBrowser
